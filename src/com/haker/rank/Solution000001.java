99bfe79df3a10ff044c7dff92fc996e0f6d00406
irakli1990 <iraklikardawa@gmail.com>
2022-01-16 11:57:26 +0100

    done solution000001

---
+++ src/com/haker/rank/Solution000001.java
@@ -0,0 +1,41 @@
+package com.haker.rank;
+
+import java.util.Scanner;
+
+//Task
+//        Given an integer, , perform the following conditional actions:
+//
+//        If n  is odd, print Weird
+//        If n  is even and in the inclusive range of 2  to 5 , print Not Weird
+//        If n  is even and in the inclusive range of 6 to 20, print Weird
+//        If n  is even and greater than 20, print Not Weird
+//        Complete the stub code provided in your editor to print whether or not  is weird
+
+public class Solution000001 {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static boolean isOdd(int value) {
+        return value % 2 != 0;
+    }
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (isOdd(N)) {
+            System.out.println("Weird");
+        } else {
+            if (2 <= N && N <= 5) {
+                System.out.println("Not Weird");
+            } else if (6 <= N && N <= 20) {
+                System.out.println("Weird");
+
+            } else {
+                System.out.println("Not Weird");
+            }
+        }
+
+        scanner.close();
+    }
+}
