bbde614d4a425c001c8d0c075c6b923368939cb2
Irakli <iraklikardawa@gmail.com>
2022-02-04 21:06:00 +0100

    add: Solution0004

---
+++ Solution0004.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
+// Given a string , print Yes if it is a palindrome, print No otherwise.
+
+public class Solution0004 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
