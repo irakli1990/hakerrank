ce7b9f74cadd11ebf7b51a877a323f4c1b2eacae
Irakli <iraklikardawa@gmail.com>
2022-02-04 20:09:50 +0100

    code reformat

--- Solution0001.java
+++ Solution0001.java
@@ -1,29 +1,25 @@
 import java.util.*;
 
-
 public class Solution0001 {
 
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
 
-     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-
-
-        System.out.println(A.length()+B.length());
+        System.out.println(A.length() + B.length());
 
         int compare = A.compareTo(B);
-        if(compare < 0){
+        if (compare < 0) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
 
-        String aCapital = A.substring(0,1).toUpperCase();
-        String bCapital = B.substring(0,1).toUpperCase();
-        
+        String aCapital = A.substring(0, 1).toUpperCase();
+        String bCapital = B.substring(0, 1).toUpperCase();
+
         System.out.println(aCapital + " " + bCapital);
     }
 
