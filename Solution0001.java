f0739005725161dc4be99d8081acf7dd7c4e36bd
Irakli <iraklikardawa@gmail.com>
2022-02-04 20:12:05 +0100

    fixed bug

--- Solution0001.java
+++ Solution0001.java
@@ -11,14 +11,14 @@ public class Solution0001 {
         System.out.println(A.length() + B.length());
 
         int compare = A.compareTo(B);
-        if (compare < 0) {
+        if (compare > 0) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
 
-        String aCapital = A.substring(0, 1).toUpperCase();
-        String bCapital = B.substring(0, 1).toUpperCase();
+        String aCapital = A.substring(0, 1).toUpperCase() + A.substring(1).toLowerCase();
+        String bCapital = B.substring(0, 1).toUpperCase() + B.substring(1).toLowerCase();
 
         System.out.println(aCapital + " " + bCapital);
     }
